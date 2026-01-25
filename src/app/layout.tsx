de1f2954d6924a607d8bfaa980a918d0cf24a64b
aniket mali <aniket@anikets-MacBook-Air.local>
2026-01-26 03:21:11 +0530

    test: new favicon asset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ export const metadata: Metadata = {
         title: "CardCRM",
     },
     icons: {
-        icon: "/icons/favicon-square.png",
-        apple: "/icons/favicon-square.png",
+        icon: "/icons/favicon-test.jpg",
+        apple: "/icons/favicon-test.jpg",
     },
 };
 
