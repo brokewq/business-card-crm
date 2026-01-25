78f5ad35f7ee9ae6f33185b3e851881225a63661
aniket mali <aniket@anikets-MacBook-Air.local>
2026-01-26 02:58:59 +0530

    feat: update favicon and PWA icons with new 3D logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ export const metadata: Metadata = {
         title: "CardCRM",
     },
     icons: {
-        icon: "/icons/icon-192.png",
-        apple: "/icons/icon-192.png",
+        icon: "/icons/logo.png",
+        apple: "/icons/logo.png",
     },
 };
 
